720bad1ddb07ae3ff1d826801bbfd26ff71aeb58
gilmarferrini <ferrinigilmar@gmail.com>
2021-12-24 09:38:29 -0300

    feat: adicionado pagina de listagem de repositorios

---
+++ src/app/repositories/pages/list-repositories/list-repositories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-repositories',
+  templateUrl: './list-repositories.component.html',
+  styleUrls: ['./list-repositories.component.scss']
+})
+export class ListRepositoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from "@angular/common/http"
 import { RepositoryService } from './services/repository.service';
+import { ListStatusComponent } from './components/list-status/list-status.component';
+import { ListRepositoriesComponent } from './pages/list-repositories/list-repositories.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListStatusComponent,
+    ListRepositoriesComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule
