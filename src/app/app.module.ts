4a3c77212744cff3e20916223ef66e2173513c94
gilmarferrini <ferrinigilmar@gmail.com>
2021-12-25 08:55:16 -0300

    refactor: movendo header para modulo de repositorios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { RepositoriesModule } from './repositories/repositories.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
     RepositoriesModule
   ],
   providers: [],
--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -1,18 +1,21 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from "@angular/common/http"
-import { RepositoryService } from './services/repository.service';
 import { ListStatusComponent } from './components/list-status/list-status.component';
 import { ListRepositoriesComponent } from './pages/list-repositories/list-repositories.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     ListStatusComponent,
-    ListRepositoriesComponent
+    ListRepositoriesComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ]
 })
 export class RepositoriesModule { }
