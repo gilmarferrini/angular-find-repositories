1a92db79c9bba917b9baeb6ef790e98a768b4e0e
gilmarferrini <ferrinigilmar@gmail.com>
2021-12-27 18:40:24 -0300

    feat: adicionando verificacao para buscar issues

--- src/app/repositories/screens/details-repository/details-repository.component.ts
+++ src/app/repositories/screens/details-repository/details-repository.component.ts
@@ -14,6 +14,8 @@ export class DetailsRepositoryComponent implements OnInit {
   repository: Repository = {} as Repository
   issues: Issue[] = [];
   total_count: number = 0;
+  currentPage: number = 1;
+  hasIssues: boolean = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -24,12 +26,50 @@ export class DetailsRepositoryComponent implements OnInit {
     this.repository = this.activatedRoute.snapshot.data['repository']
     console.log(this.repository)
 
+    this.loadIssues(this.repository.owner.login, this.repository.name, 1)
+  }
+
+  loadIssues(ownerName: string, repositoryName: string, page: number) {
     this.repositoryService
-      .getIssues(this.repository.owner.login, this.repository.name)
+      .getIssues(ownerName, repositoryName, page)
       .subscribe(data => {
         this.total_count = data.total_count
         this.issues = data.items;
+
+        if (this.issues.length < 30) {
+          this.hasIssues = false;
+        } else {
+          this.hasIssues = true;
+        }
       })
+  }
+
+  previousPage() {
+    if (this.currentPage === 1) {
+      return;
+    }
+
+    this.currentPage--;
+
+    this.loadIssues(this.repository.owner.login, this.repository.name, this.currentPage)
+  }
+
+  nextPage() {
+
+    if (
+      this.issues.length === 0 ||
+      this.issues.length < 30 ||
+      this.currentPage === this.getTotalPages()
+    ) {
+      return;
+    }
+
+    this.currentPage++;
+    this.loadIssues(this.repository.owner.login, this.repository.name, this.currentPage)
+  }
+
+  getTotalPages() {
+    return Math.floor(this.total_count / 30)
   }
 
 }
